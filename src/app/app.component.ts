ec8cc048e75bba596719e52dff1b900bf508f8a5
ignacioborello97 <nachoborello97@gmail.com>
2020-11-26 18:24:39 -0300

    commit inicial con ejercicio de los puntos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
 import { Component } from '@angular/core';
 
+interface Dot {
+    active: boolean
+    trail: boolean
+}
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'prueba-tecnica';
+    dots: Dot[] = [
+        { active: false, trail: false },
+        { active: false, trail: false },
+        { active: false, trail: false },
+        { active: false, trail: false }
+    ]
+
+    dot_selection_handler(index) {
+        for(let dot of this.dots) {
+            if(dot.trail){
+                dot.trail = false;
+            }
+            if(dot.active){
+                dot.active = false;
+                dot.trail = true;
+            }
+        }
+        this.dots[index].trail = false;
+        this.dots[index].active = true;
+    }
 }
